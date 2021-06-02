65307ca449730fa7a02953151ba08774f02e3c0a
lkappel <lucaskappel.jp@gmail.com>
2021-06-02 16:06:42 -0500

    Added a testing method

---
+++ TutorWorkspace/TestingGrounds.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TutorWorkspace
+{
+    class TestingGrounds
+    {
+        static void TestMethod()
+        {
+            // Test code
+        }
+    } // End class TestingGropunds
+} // End namespace TutorWorkspace
